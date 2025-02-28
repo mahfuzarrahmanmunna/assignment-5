398bd517f0170d7e2d3d3ab967325ac90c83d9c5
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-03-01 03:39:42 +0600

    added new js file in my webpage

---
+++ script/toggole.js
@@ -0,0 +1,19 @@
+function indexHtml() {
+    const btn = document.getElementById('discover-something');
+    if (btn) {
+        btn.addEventListener('click', () => {
+            window.location.href = '/blog.html'
+        })
+    }
+}
+indexHtml()
+
+function blogHtml() {
+    const btn = document.getElementById('back-to-desk-btn');
+    if (btn) {
+        btn.addEventListener('click', () => {
+            window.location.href = './index.html'
+        })
+    }
+}
+blogHtml()
